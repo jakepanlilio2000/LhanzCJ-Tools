6ac7edf466b46214e24f9f1ebcf0acd42e548b2c
jakepanlilio2000 <jakepanlilio@gmail.com>
2025-02-26 00:13:03 +0800

    Simple technicial tool

---
+++ LhanzCJ Installer/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LhanzCJ_Installer
+{
+    partial class LhanzCJ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LhanzCJ));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dxdiag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button6
+            // 
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // dxdiag
+            // 
+            resources.ApplyResources(this.dxdiag, "dxdiag");
+            this.dxdiag.Name = "dxdiag";
+            this.dxdiag.UseVisualStyleBackColor = true;
+            this.dxdiag.Click += new System.EventHandler(this.dxdiag_Click);
+            // 
+            // LhanzCJ
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Controls.Add(this.dxdiag);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LhanzCJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button dxdiag;
+    }
+}
+
---
+++ LhanzCJ Installer/Form1.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+
+namespace LhanzCJ_Installer
+{
+
+    public partial class LhanzCJ : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
+        public LhanzCJ()
+        {
+            InitializeComponent();
+            CheckAdminPrivileges();
+        }
+        private void CheckAdminPrivileges()
+        {
+            bool isAdmin = IsAdministrator();
+
+            button5.Enabled = isAdmin; 
+            label1.Visible = !isAdmin;
+            button7.Enabled = isAdmin;
+
+        }
+
+        private bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL desk.cpl,,0");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open Desktop Icon Settings: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IntPtr oldValue = IntPtr.Zero;
+            bool redirectionDisabled = false;
+
+            try
+            {
+                // Disable redirection for 32-bit apps on 64-bit Windows
+                if (Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess)
+                {
+                    redirectionDisabled = Wow64DisableWow64FsRedirection(ref oldValue);
+                }
+
+                // Use the direct path to OptionalFeatures.exe
+                string exePath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.System),
+                    "OptionalFeatures.exe"
+                );
+                Process.Start(exePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open Windows Features: " + ex.Message, "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (redirectionDisabled)
+                {
+                    Wow64RevertWow64FsRedirection(oldValue);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IntPtr oldValue = IntPtr.Zero;
+            bool redirectionDisabled = false;
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c net use \\\\technical",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = psi })
+                {
+                    process.Start();
+
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    if (!string.IsNullOrWhiteSpace(error))
+                    {
+                        if (error.Contains("The network path was not found"))
+                        {
+                            richTextBox1.Clear();
+                            richTextBox1.Text = "Error: The network path was not found.\n\n" +
+                                                "Possible Fix:\n" +
+                                                "1. Enable SMB 1.0/CIFS File Sharing Support.\n" +
+                                                "2. Open Windows Features\n" +
+                                                "3. Check 'SMB 1.0/CIFS File Sharing Support' and click OK.\n" +
+                                                "4. Restart your computer.\n\n" +
+                                                "Opening Windows Features...\n";
+
+                            if (Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess)
+                            {
+                                redirectionDisabled = Wow64DisableWow64FsRedirection(ref oldValue);
+                            }
+                            string exePath = Path.Combine(
+                                Environment.GetFolderPath(Environment.SpecialFolder.System),
+                                "OptionalFeatures.exe"
+                            );
+                            Process.Start(exePath);
+
+                        }
+                        else
+                        {
+                            richTextBox1.Text = "Error: " + error;
+                        }
+                    }
+                    else
+                    {
+                        richTextBox1.Text = output;
+                    }
+
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Failed to open network location: " + ex.Message;
+            }
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("ms-settings:windowsupdate");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open Windows Update: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            InstallPrograms();
+        }
+
+        private void InstallPrograms()
+        {
+            richTextBox1.Clear();
+            string[] commands = new string[]
+                {
+                    "echo Checking if App Installer is installed...",
+
+                    // Check for Winget and install it if necessary
+                    "where winget >nul 2>nul && echo App Installer is already installed. || (echo Installing App Installer... && powershell -Command \"$ProgressPreference = 'SilentlyContinue'; irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex\")",
+
+                    "echo Installing 7zip...",
+                    "winget install -e --id 7zip.7zip --silent --accept-source-agreements",
+
+                    "echo Installing Google Chrome...",
+                    "winget install -e --id Google.Chrome --silent --accept-source-agreements",
+
+                    "echo Installing Zoom...",
+                    "winget install -e --id Zoom.Zoom --silent --accept-source-agreements",
+
+                    "echo Installing Acrobat Reader...",
+                    "winget install -e --id Adobe.Acrobat.Reader.64-bit --silent --accept-source-agreements",
+
+                    "echo Installing VLC...",
+                    "winget install -e --id VideoLAN.VLC --silent --accept-source-agreements",
+
+                    "echo Installing Visual C++ Redistributables...",
+                    "winget install -e --id Microsoft.VCRedist.2005.x86 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2005.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2008.x86 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2008.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2010.x86 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2010.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2012.x86 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2012.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2013.x86 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2013.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2015+.x64 --silent --accept-source-agreements",
+                    "winget install -e --id Microsoft.VCRedist.2015+.x86 --silent --accept-source-agreements",
+
+                    "echo Installing DirectX...",
+                    "curl -o directx.exe https://download.microsoft.com/download/8/4/a/84a35bf1-dafe-4ae8-82af-ad2ae20b6b14/directx_Jun2010_redist.exe",
+                    "mkdir C:\\DirectX",
+                    "directx.exe /Q /T:C:\\DirectX",
+                    "C:\\DirectX\\DXSETUP.exe /silent && echo DirectX installation successful! || echo DirectX installation failed.",
+
+                    "echo.",
+                    "echo Installation completed successfully."
+                };
+
+
+
+            progressBar1.Maximum = commands.Length;
+            progressBar1.Value = 0;
+
+            Thread thread = new Thread(() =>
+            {
+                foreach (string command in commands)
+                {
+                    string output = RunCommand(command);
+
+                    Invoke(new Action(() =>
+                    {
+                        UpdateRichTextBox(command, output);
+                        progressBar1.Value += 1;
+                    }));
+                }
+            });
+
+            thread.Start();
+        }
+
+        private string RunCommand(string command)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c " + command,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = psi })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    return !string.IsNullOrWhiteSpace(error) ? error : output;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        private void UpdateRichTextBox(string command, string output)
+        {
+            if (command.Contains("winget install"))
+            {
+                // Extract app name from the command (remove 'winget install -e --id ')
+                string appName = command.Replace("winget install -e --id ", "").Replace("--silent", "").Replace("--accept-source-agreements", "").Trim();
+
+                if (output.Contains("Successfully installed"))
+                {
+                    richTextBox1.AppendText($"{appName} installed successfully.\n\n");
+                }
+                else if (output.Contains("already installed"))
+                {
+                    richTextBox1.AppendText($"{appName} is already installed.\n\n");
+                }
+                else
+                {
+                    richTextBox1.AppendText($"Failed to install {appName}.\n\n");
+                }
+            }
+            else if (command.Contains("echo"))
+            {
+                richTextBox1.AppendText(output + "\n"); // Display regular messages
+            }
+        }
+
+
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://www.office.com/setup/", 
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open the website: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            IntPtr oldValue = IntPtr.Zero;
+            bool redirectionDisabled = false;
+
+            try
+            {
+                // Disable redirection for 32-bit apps on 64-bit Windows
+                if (Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess)
+                {
+                    redirectionDisabled = Wow64DisableWow64FsRedirection(ref oldValue);
+                }
+
+                // Use the direct path to slui.exe
+                string exePath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.System),
+                    "slui.exe"
+                );
+                Process.Start(exePath, "3");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open activation program: " + ex.Message, "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Re-enable redirection if it was disabled
+                if (redirectionDisabled)
+                {
+                    Wow64RevertWow64FsRedirection(oldValue);
+                }
+            }
+        }
+
+        private void dxdiag_Click(object sender, EventArgs e)
+        {
+            IntPtr oldValue = IntPtr.Zero;
+            bool redirectionDisabled = false;
+
+            try
+            {
+                if (Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess)
+                {
+                    redirectionDisabled = Wow64DisableWow64FsRedirection(ref oldValue);
+                }
+
+                string exePath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.System),
+                    "dxdiag.exe"
+                );
+                Process.Start(exePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open activation program: " + ex.Message, "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (redirectionDisabled)
+                {
+                    Wow64RevertWow64FsRedirection(oldValue);
+                }
+            }
+        }
+    }
+}
